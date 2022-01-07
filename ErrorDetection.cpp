5d600570edd570cd88987e7d1a8a724fabf41d6a
sadjad <asadisadjad@gmail.com>
2022-01-08 00:56:20 +0330

    ErrorDetection *.cpp update

--- ErrorDetection.cpp
+++ ErrorDetection.cpp
@@ -1,20 +1,15 @@
-// ErrorDetection.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
+﻿#include"Header.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	clock_t start = clock();
+	// first step you must create memory with random binary number 
+	//create_memory(memFileAddr); 
+
+	bool mem[rows][cols] = {};
+	read_memory(mem, memFileAddr);
+
+	clock_t end = clock();
+	cout << "\nElapsed Time : " << (double)(end-start) / CLOCKS_PER_SEC << " second" << "\n\n" << "finish";
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file

cbd17de4aa2101e7a19c3f10104baf617aba38c9
sadjad <asadisadjad@gmail.com>
2022-01-08 11:18:50 +0330

    odd parity add to Parity.cpp

--- Header.h
+++ Header.h
@@ -11,12 +11,22 @@
 using namespace std;
 
 
-#define memFileAddr "mem.bin"
+#define MEM_FILE_ADDR "mem.bin"
 #define LmemFileAddr "Lmem.bin"
-#define rows 16384  //4 
-#define cols 64  //4 
+#define ROWS 16384  //4 
+#define COLS 64  //4 
+
+#define PARITY_No 8  //number of parity bit
+#define CRC_No 33
+#define FAULT_No 4   //number of error bit injection
+
 
 
 bool create_memory(string addr);
-bool read_memory(bool mem[rows][cols], string addr);
+bool read_memory(bool mem[ROWS][COLS], string addr);
 
+float even_parity(bool org_mem[ROWS][COLS], bool CEPB[ROWS][COLS + PARITY_No], bool FEPB[ROWS][COLS + PARITY_No], bool burst);
+float odd_parity(bool org_mem[ROWS][COLS], bool COPB[ROWS][COLS + PARITY_No], bool FOPB[ROWS][COLS + PARITY_No], bool burst);
+
+
+bool  fault_inject(int fault_P[][FAULT_No], int rows, int cols, bool burst);
--- Parity.cpp
+++ Parity.cpp
@@ -125,5 +125,132 @@ float even_parity(bool org_mem[ROWS][COLS], bool CEPB[ROWS][COLS + PARITY_No], b
 	return (counter * 100 / (float)(ROWS));
 }
 
+float odd_parity(bool org_mem[ROWS][COLS], bool COPB[ROWS][COLS + PARITY_No], bool FOPB[ROWS][COLS + PARITY_No], bool burst)
+{
+	// calc odd parity befor error inject
+
+	bool parity[PARITY_No] = {};
+
+	//cout << "\n----Odd Parity";
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			if (!(j % (COLS / PARITY_No)))
+			{
+				parity[j / (COLS / PARITY_No)] = org_mem[i][j] ^ false;
+			}
+			else
+			{
+				parity[j / (COLS / PARITY_No)] ^= org_mem[i][j];
+			}
+
+			COPB[i][j] = org_mem[i][j];
+			FOPB[i][j] = org_mem[i][j];
+
+		}
+		for (int k = 0; k < PARITY_No; k++)
+		{
+			COPB[i][COLS + k] = !parity[k];
+			FOPB[i][COLS + k] = !parity[k];
+			
+			//check!
+			//cout << COPB[i][COLS + k];
+		}
+	}
+
+	//check!
+
+	/*for (int i = 0;i < ROWS;i++)
+	{
+		cout << endl;
+		for (int j = 0;j < COLS + PARITY_No;j++)
+		{
+			cout << COPB[i][j];
+		}
+
+	}*/
+	cout << "\n\n  >Correct Odd Parity (COP) Finished\n\n";
+	//========================================================================================================================
+	
+	//inject error to correct data
+	int fault_P[ROWS][FAULT_No] = {};
+	fault_inject(fault_P, ROWS, COLS + PARITY_No, burst);
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < FAULT_No; j++)
+		{
+			FOPB[i][fault_P[i][j]] = !(FOPB[i][fault_P[i][j]]);
+		}
+	}
+
+	// check !
+	/*for (int i = 0;i < ROWS;i++)
+	{
+		for (int j = 0; j < COLS + PARITY_No; j++)
+		{
+			cout << FOPB[i][j];
+		}
+		cout << endl;
+	}*/
+	cout << "\n\n  >Fault Odd Parity (FOP) Finished\n\n";
+
+	//=========================================================================================================================
+	
+	//calc odd parity after error injection
+
+	bool BFparity[PARITY_No] = {};
+	bool AFparity[ROWS][PARITY_No] = {};
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			if (!(j % (COLS / PARITY_No)))
+			{
+				BFparity[j / (COLS / PARITY_No)] = FOPB[i][j] ^ false;
+			}
+			else
+			{
+				BFparity[j / (COLS / PARITY_No)] ^= FOPB[i][j];
+			}
+		}
+		for (int k = 0; k < PARITY_No; k++)
+		{
+			AFparity[i][k] = !BFparity[k];
+			
+			//check!
+			//cout << AFparity[i][k];
+		}
+	}
+
+	//check!
+	/*for (int i = 0;i < ROWS;i++)
+	{
+		for (int j = COLS;j < COLS + PARITY_No;j++)
+		{
+			cout << AFparity[i][j - COLS] << " | " << FOPB[i][j] << "  -  ";
+		}
+		cout << endl;
+	}*/
+	//====================================================================================================================================
+	
+	//calc probability of error detection by odd parity on this configure
+	int counter = 0;
+	for (int i = 0; i < ROWS; i++)
+	{
+		bool flag = false;
+		for (int j = COLS; j < COLS + PARITY_No; j++)
+		{
+			if (!(!(AFparity[i][j - COLS] ^ FOPB[i][j])))
+			{
+				flag = true;
+				break;
+			}
+		}
+		if (flag)	counter++;
+	}
+	return (counter * 100 / (float)(ROWS));
+}
+
 
 
